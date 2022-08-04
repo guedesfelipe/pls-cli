63ade95b7f6f9fc3a25ae03b4510e1771752cd31
Alonso Melgar Lopez <alonmar3@gmail.com>
2022-08-03 21:49:38 -0500

    ✅ Update read quotes

--- pls_cli/utils/quotes.py
+++ pls_cli/utils/quotes.py
@@ -3,12 +3,14 @@ import os
 import random
 
 
-def get_rand_quote(lang: str):
+def get_rand_quote():
     __location__ = os.path.realpath(
         os.path.join(os.getcwd(), os.path.dirname(__file__))
     )
     with open(
-        os.path.join(__location__, f'{lang}/quotes.json'), 'r', encoding='utf-8'
+        os.path.join(__location__, f'quotes.json'),
+        'r',
+        encoding='utf-8',
     ) as quotes_file:
         list_of_quotes = json.load(quotes_file)
     return random.choice(list_of_quotes)
